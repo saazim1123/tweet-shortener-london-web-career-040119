d01ba8302000a41b480d8770de791a5b6a401936
Adnan Azim <saazim1123@gmail.com>
2019-03-20 05:29:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+
+=begin
+"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
+"Hey guys, can anyone teach me how (2) (b) cool? I really want (2) b the best (@) everything, u know what I mean? Tweeting is super fun u guys!!!!"
+
+=end
+
+def word_substituter(tweet)
+  words = []
+  tweet_one = tweet.split(" ")
+  tweet_one.each do |word|
+    case word.downcase
+    
+    when "to"
+      words << "2"
+      
+    when "be"
+      words << "b"
+      
+    when "at"
+      words << "@"
+      
+    end
+  end
+  
+end
